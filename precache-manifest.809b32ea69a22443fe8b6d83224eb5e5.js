714ab9e0c45c3633e6de007a5df6caf828510820
Abbas Egbeyemi <abbasegbeyemi@gmail.com>
2020-09-17 14:46:25 +0100

    Updates

---
+++ precache-manifest.809b32ea69a22443fe8b6d83224eb5e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c234be2da00eed05f01f5e0e92ed44d1",
+    "url": "/project-nautilus/index.html"
+  },
+  {
+    "revision": "7f7afa8abd1305560ec8",
+    "url": "/project-nautilus/static/css/main.f414ece1.chunk.css"
+  },
+  {
+    "revision": "616b974a01b0651f14a0",
+    "url": "/project-nautilus/static/js/2.953cbe66.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project-nautilus/static/js/2.953cbe66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7afa8abd1305560ec8",
+    "url": "/project-nautilus/static/js/main.71d1623c.chunk.js"
+  },
+  {
+    "revision": "1d1168af404940c5af9b",
+    "url": "/project-nautilus/static/js/runtime-main.5c29ba9e.js"
+  }
+]);
